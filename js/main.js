2e69eb95fa4a14c58bf71c6792000fe6d283067d
cedwinpp <155151388+cedwinpp@users.noreply.github.com>
2025-07-22 00:41:38 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Animación de Scroll para revelar elementos
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150; // Distancia para que el elemento sea visible
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Llama a la función al cargar para mostrar los elementos ya visibles
+reveal();
